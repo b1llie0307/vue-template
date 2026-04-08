2a9c2c572de14757954b0e5dc70fd813c5e3f5f1
b1llie0307 <2298563686@qq.com>
2026-04-08 09:16:48 +0800

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
 import App from './App.vue'
 import './assets/main.css'
 // import {i18n} from './locales/i18n.js'
 // createApp(App).use(i18n).mount('#app') // 注入国际化函数$t
 
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus);
+app.mount('#app');
